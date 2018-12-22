f879e854ffd3b79c5aacbe386a9e6a97df8dedec
Peter Westmacott <peter.westmacott@gmail.com>
2018-12-22 20:06:49 +0000

    Switch to Workman layout.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -152,10 +152,10 @@ enum { PRIMARY, NUMPAD, FUNCTION }; // layers
   *
   */
 
-#define PRIMARY_KEYMAP_QWERTY
+// #define PRIMARY_KEYMAP_QWERTY
 // #define PRIMARY_KEYMAP_COLEMAK
 // #define PRIMARY_KEYMAP_DVORAK
-// #define PRIMARY_KEYMAP_CUSTOM
+#define PRIMARY_KEYMAP_CUSTOM
 
 
 
@@ -217,19 +217,20 @@ KEYMAPS(
    ShiftToLayer(FUNCTION)),
 
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
-  // Edit this keymap to make a custom layout
+  // Alphabetic keys are the Workman Layout.
+  // Non-alphabetic keys, well, they have to move anyway because the physical layout is non-standard.
   [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+  (___,           Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick,  Key_Q, Key_D, Key_R, Key_W, Key_B, Key_Tab,
+   Key_Backslash, Key_A, Key_S, Key_H, Key_T, Key_G,
+   ___,           Key_Z, Key_X, Key_M, Key_C, Key_V, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_Enter,     Key_J, Key_F, Key_U,     Key_P,         Key_Semicolon, Key_Equals,
+                  Key_Y, Key_N, Key_E,     Key_O,         Key_I,         Key_Quote,
+   Key_RightAlt,  Key_K, Key_L, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
