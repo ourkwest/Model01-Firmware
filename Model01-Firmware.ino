22c5b3afbce1786fc5ad915431fa93c76b76152a
Peter Westmacott <peter.westmacott@gmail.com>
2018-12-23 20:07:02 +0000

    Add some keys to the Navigation layer.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -245,17 +245,17 @@ KEYMAPS(
 
 
   [NAVIGATION] =  KEYMAP_STACKED
-  (___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___, ___, ___, ___,
-   ___, ___, ___, ___,
+  (___, ___,        ___,          ___,     ___,       ___, ___,
+   ___, ___,        ___,          ___,     ___,       ___, ___,
+   ___, Key_Escape, Key_Spacebar, Key_Tab, Key_Enter, ___,
+   ___, ___,        ___,          ___,     ___,       ___, ___,
+   ___, ___,        ___,          ___,
    ___,
 
-   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
-   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
-                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         ___,
-   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___,           ___,           ___,         ___,            ___,
+   ___, ___, ___,           ___,           ___,         ___,            ___,
+        ___, Key_LeftArrow, Key_DownArrow, Key_UpArrow, Key_RightArrow, ___,
+   ___, ___, ___,           Key_PageDown,  Key_PageUp,  ___,            ___,
    ___, ___, ___, ___,
    LockLayer(NAVIGATION)),
 
